68472fdbe3beed9338734e81c2c2aea849cd38c3
unknown <mikeber000@gmail.com>
2024-05-31 21:05:53 +0800

    stopped on company page

--- dist/js/script.js
+++ dist/js/script.js
@@ -58,34 +58,51 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   search: () => (/* binding */ search)
 /* harmony export */ });
 const search = () => {
-  const btns = document.querySelectorAll('.search__btn');
-  const pointer = document.querySelector('.search__pointer');
-  const form = document.querySelector('.search__form');
-  const input = document.querySelector('.search__input');
-  const wrapper = document.querySelector('.search__options');
-  form.addEventListener('submit', e => {
-    e.preventDefault();
-  });
-  btns.forEach(btn => {
-    btn.addEventListener('click', () => {
-      // console.log(btn.getBoundingClientRect().left);
-      resetActiveBtn();
-      btn.parentNode.classList.add('search__item--active');
-      pointer.style.left = btn.getBoundingClientRect().left - wrapper.getBoundingClientRect().left - pointer.getBoundingClientRect().width + btn.getBoundingClientRect().width / 2 + 10 + 'px';
-      input.value = btn.textContent;
+  try {
+    const btns = document.querySelectorAll('.search__btn');
+    const pointer = document.querySelector('.search__pointer');
+    const form = document.querySelector('.search__form');
+    const input = document.querySelector('.search__input');
+    const wrapper = document.querySelector('.search__options');
+    const inputBtns = document.querySelector('.search__input-btns');
+    const resetInput = document.querySelector('.search__remove');
+    form.addEventListener('submit', e => {
+      e.preventDefault();
     });
-  });
-  function resetActiveBtn() {
+    input.addEventListener('input', e => {
+      inputBtns.classList.remove('hide');
+      if (!e.target.value) {
+        inputBtns.classList.add('hide');
+      }
+    });
+    resetInput.addEventListener('click', () => {
+      input.value = '';
+      inputBtns.classList.add('hide');
+    });
     btns.forEach(btn => {
-      btn.parentNode.classList.remove('search__item--active');
+      btn.addEventListener('click', () => {
+        // console.log(btn.getBoundingClientRect().left);
+        inputBtns.classList.remove('hide');
+        resetActiveBtn();
+        btn.parentNode.classList.add('search__item--active');
+        pointer.style.left = btn.getBoundingClientRect().left - wrapper.getBoundingClientRect().left - pointer.getBoundingClientRect().width + btn.getBoundingClientRect().width / 2 + 10 + 'px';
+        input.value = btn.textContent;
+      });
     });
+    function resetActiveBtn() {
+      btns.forEach(btn => {
+        btn.parentNode.classList.remove('search__item--active');
+      });
+    }
+
+    // window.addEventListener('resize', () => {
+    //     console.log(wrapper.getBoundingClientRect().left);
+    // })
+
+    // console.log(pointer.getBoundingClientRect().left)
+  } catch (error) {
+    console.log(error);
   }
-
-  // window.addEventListener('resize', () => {
-  //     console.log(wrapper.getBoundingClientRect().left);
-  // })
-
-  // console.log(pointer.getBoundingClientRect().left)
 };
 
 /***/ })
--- src/assets/js/search.js
+++ src/assets/js/search.js
@@ -1,18 +1,34 @@
 export const search = () => {
-    const btns = document.querySelectorAll('.search__btn');
+    try {
+        const btns = document.querySelectorAll('.search__btn');
     const pointer = document.querySelector('.search__pointer');
     const form = document.querySelector('.search__form');
     const input = document.querySelector('.search__input');
     const wrapper = document.querySelector('.search__options');
-
+    const inputBtns = document.querySelector('.search__input-btns');
+    const resetInput = document.querySelector('.search__remove');
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
     })
 
+    input.addEventListener('input', (e) => {
+        inputBtns.classList.remove('hide');
+        if(!e.target.value) {
+            inputBtns.classList.add('hide');
+        }
+    })
+
+    resetInput.addEventListener('click', () => {
+        input.value = '';
+        inputBtns.classList.add('hide');
+    })
+
+
     btns.forEach(btn => {
         btn.addEventListener('click', () => {
             // console.log(btn.getBoundingClientRect().left);
+            inputBtns.classList.remove('hide');
             resetActiveBtn();
             btn.parentNode.classList.add('search__item--active');
             pointer.style.left = btn.getBoundingClientRect().left - wrapper.getBoundingClientRect().left - pointer.getBoundingClientRect().width + btn.getBoundingClientRect().width / 2 + 10 + 'px';
@@ -30,4 +46,8 @@ export const search = () => {
     // })
 
     // console.log(pointer.getBoundingClientRect().left)
+    } catch (error) {
+        console.log(error)
+    }
+    
 }
